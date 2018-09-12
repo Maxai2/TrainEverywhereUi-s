fbed21c8402af19b372a34dfcd4be45839f1ad86
Хамелеон <maxali946@gmail.com>
2018-09-12 18:14:09 +0400

    35%

--- TrainEverywhere/TrainEverywhere/NewsPage.xaml.cs
+++ TrainEverywhere/TrainEverywhere/NewsPage.xaml.cs
@@ -31,45 +31,68 @@ namespace TrainEverywhere
             InitializeComponent();
             BindingContext = this;
 
-            string data = GetHtmlPageText("https://www.bodybuilding.com/fun/whats-new.html");
-
-            string tag = "a";
-            string pattern = string.Format(@"\<{0}.*?\>(?<tegData>.+?)\<\/{0}\>", tag.Trim());
-
-            Regex regex = new Regex(pattern, RegexOptions.ExplicitCapture);
-            MatchCollection matches = regex.Matches(data);
-
-            foreach (Match matche in matches)
+            var news1 = new News
             {
-                News.Add(new News
-                {
-                    Description = matche.Groups["tegData"].Value
-                });
-            }
-        }
+                Description = "Be an everyday beast with a complete pre - workout meal that hits your training macros and a killer shoulder workout to build size and mass.",
+                Image = "https://www.bodybuilding.com/images/2018/september/everyday-beast-salmon-burger-and-shoulder-workout-header-600x338.jpg",
+                Title = "Everyday Beast: Salmon Burger and Shoulder Workout"
+            };
 
-        public static string GetHtmlPageText(string url)
-        {
-            WebClient client = new WebClient();
-
-            using (Stream data = GenerateStreamFromString(client.DownloadString(url)))
+            var news2 = new News
             {
-                using (StreamReader reader = new StreamReader(data))
-                {
-                    return reader.ReadToEnd();
-                }
-            }
+                Description = "Want better shoulders? Signature team athletes Tanner Hobbs, Meg Squats, Rodney Razor, and Brian Casad reveal their favorite delt-building workout.",
+                Image = "https://www.bodybuilding.com/images/2018/august/4-workouts-that-are-insanely-effective-at-building-shoulders-header-600x338.jpg",
+                Title = "4 Workouts That Are Insanely Effective At Building Shoulders"
+            };
+
+            News.Add(news1);
+            News.Add(news2);
+
+            //WebClient client = new WebClient();
+
+            ////client.UseDefaultCredentials = true;
+            //client.Proxy.Credentials = System.Net.CredentialCache.DefaultCredentials;
+
+
+            //string data = client.DownloadString("https://www.bodybuilding.com/fun/whats-new.html");
+
+            //string tag = "a";
+            //string pattern = string.Format(@"\<{0}.*?\>(?<tegData>.+?)\<\/{0}\>", tag.Trim());
+
+            //Regex regex = new Regex(pattern, RegexOptions.ExplicitCapture);
+            //MatchCollection matches = regex.Matches(data);
+
+            //foreach (Match matche in matches)
+            //{
+            //    News.Add(new News
+            //    {
+            //        Description = matche.Groups["tegData"].Value
+            //    });
+            //}
         }
 
-        public static Stream GenerateStreamFromString(string s)
-        {
-            var stream = new MemoryStream();
-            var writer = new StreamWriter(stream);
-            writer.Write(s);
-            writer.Flush();
-            stream.Position = 0;
-            return stream;
-        }
+        //public static string GetHtmlPageText(string url)
+        //{
+        //    WebClient client = new WebClient();
+
+        //    using (Stream data = GenerateStreamFromString(client.DownloadString(url)))
+        //    {
+        //        using (StreamReader reader = new StreamReader(data))
+        //        {
+        //            return reader.ReadToEnd();
+        //        }
+        //    }
+        //}
+
+        //public static Stream GenerateStreamFromString(string s)
+        //{
+        //    var stream = new MemoryStream();
+        //    var writer = new StreamWriter(stream);
+        //    writer.Write(s);
+        //    writer.Flush();
+        //    stream.Position = 0;
+        //    return stream;
+        //}
 
         private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
         {
