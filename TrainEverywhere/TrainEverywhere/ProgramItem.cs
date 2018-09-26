e1b9a7a2250fd95c6a94c08c8ec4a6128c37b9d2
Хамелеон <maxali946@gmail.com>
2018-09-26 17:53:47 +0400

    80%

---
+++ TrainEverywhere/TrainEverywhere/ProgramItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrainEverywhere
+{
+    public class ProgramItem
+    {
+        public string Title { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
